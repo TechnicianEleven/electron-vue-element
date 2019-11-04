242bba6969699678a41c38d78e1d89a488138898
maoshiba18 <1039849632@qq.com>
2019-11-04 15:40:42 +0800

    electron vue element

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="wrapper">
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style  scope lang="scss">
+#wrapper{
+  background: red
+}
+</style>
